7a3bb6fefdf7f5eac0a55faa150a689aaf3937f6
eduFDiaz <fernandez9000@gmail.com>
2024-07-02 16:21:17 -0700

    deleted utils

--- server/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export type EmojiResponse = string[];
-
-export function getEmojis(): EmojiResponse {
-  let emojis: string[] = [];
-  emojis.push('😀');
-  emojis.push('😳');
-  emojis.push('🙄');
-  return emojis;
-}
